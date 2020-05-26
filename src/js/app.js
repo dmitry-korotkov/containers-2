8d58cbcef1bda88539893e5ee686ec8eee36f157
Dmitry Korotkov <korotkov.dmitriy10.1@gmail.com>
2020-05-26 23:46:02 +0400

    app and test files modified

--- src/js/app.js
+++ src/js/app.js
@@ -1,15 +1,17 @@
 export default class ErrorRepository {
-    constructor(){
-      this.errorContainer = new Map()
-    }
-    addError (errorCode, errorText) {
-      this.errorContainer.set(errorCode, errorText);
-    }
-    translate(errorCode) {
-      const errorText = this.errorContainer.get(errorCode);
-      if (errorText === null || errorText === undefined) {
-        return 'Unknown error'
-      }
-      return errorText
-    }
+  constructor() {
+    this.errorContainer = new Map();
   }
+
+  addError(errorCode, errorText) {
+    this.errorContainer.set(errorCode, errorText);
+  }
+
+  translate(errorCode) {
+    const errorText = this.errorContainer.get(errorCode);
+    if (errorText === null || errorText === undefined) {
+      return 'Unknown error';
+    }
+    return errorText;
+  }
+}
--- src/tests/test.js
+++ src/tests/test.js
@@ -1,16 +1,15 @@
-import ErrorRepository  from '../js/app';
+import ErrorRepository from '../js/app';
 
-test("get correct error", ()=> {
-    const someErrorRepository = new ErrorRepository();
-    someErrorRepository.addError(1, "Ошибка 1");
-    const result = someErrorRepository.translate(1);
-    expect(result).toBe("Ошибка 1");
-})
+test('get correct error', () => {
+  const someErrorRepository = new ErrorRepository();
+  someErrorRepository.addError(1, 'Ошибка 1');
+  const result = someErrorRepository.translate(1);
+  expect(result).toBe('Ошибка 1');
+});
 
-test("get uncorrect error", ()=> {
-    const someErrorRepository = new ErrorRepository();
-    someErrorRepository.addError(1, "Ошибка 1");
-    const result = someErrorRepository.translate(2);
-    expect(result).toBe('Unknown error');
-})
-
+test('get uncorrect error', () => {
+  const someErrorRepository = new ErrorRepository();
+  someErrorRepository.addError(1, 'Ошибка 1');
+  const result = someErrorRepository.translate(2);
+  expect(result).toBe('Unknown error');
+});
