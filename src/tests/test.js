dbb5dd67b8c76de7e4872482d19ba192f8a961e3
Dmitry Korotkov <korotkov.dmitriy10.1@gmail.com>
2020-06-02 22:17:23 +0400

    add two tests

--- src/tests/test.js
+++ src/tests/test.js
@@ -7,6 +7,22 @@ test('get correct error', () => {
   expect(result).toBe('Ошибка 1');
 });
 
+test('get size of errors', () => {
+  const someErrorRepository = new ErrorRepository();
+  someErrorRepository.addError(1, 'Ошибка 1');
+  someErrorRepository.addError(2, 'Ошибка 2');
+  const result = someErrorRepository.errorContainer.size;
+  expect(result).toBe(2);
+});
+
+test('get existence of error', () => {
+  const someErrorRepository = new ErrorRepository();
+  someErrorRepository.addError(1, 'Ошибка 1');
+  someErrorRepository.addError(2, 'Ошибка 2');
+  const result = someErrorRepository.errorContainer.has(3);
+  expect(result).toBe(false);
+});
+
 test('get uncorrect error', () => {
   const someErrorRepository = new ErrorRepository();
   someErrorRepository.addError(1, 'Ошибка 1');
